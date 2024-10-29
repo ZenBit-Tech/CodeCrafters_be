2da78ad55b534ca3aacdf77a60df9cd29043c19d
Parle648 <sashahoumi@gmail.com>
2024-10-29 11:35:32 +0200

    + Create mailer logic

--- src/common/mailer/mailer.controller.ts
+++ src/common/mailer/mailer.controller.ts
@@ -1,8 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 import { MailerService } from './mailer.service';
 
 @Controller('mailer')
 export class MailerController {
   constructor(private readonly mailerService: MailerService) {}
+
+  @Post('/invitation')
+  async sendMail() {
+    const mailDto = {
+      from: { name: 'codecrafters', address: 'codecrafters@mail.com' },
+      recipients: [{ name: 'john', address: 'morozovalex842@gmail.com' }],
+      subject: 'invitation link',
+      html: '<a href="http://asd">invitation link</a>',
+      placeholderReplacements: {},
+    };
+
+    return this.mailerService.sendEmail(mailDto);
+  }
 }
--- src/common/mailer/mailer.service.ts
+++ src/common/mailer/mailer.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
+import { MailOptions } from 'nodemailer/lib/json-transport';
+import { Address } from 'nodemailer/lib/mailer';
+
+interface MailDto {
+  from?: Address;
+  recipients: Address[];
+  subject: string;
+  html: string;
+  text?: string;
+  placeholderReplacements: Record<string, string>;
+}
 
 @Injectable()
-export class MailerService {}
+export class MailerService {
+  constructor(private readonly configService: ConfigService) {}
+  mailTransport() {
+    return nodemailer.createTransport({
+      host: this.configService.getOrThrow<string>('MAIL_HOST'),
+      port: this.configService.getOrThrow<number>('MAIL_PORT'),
+      secure: false,
+      auth: {
+        user: this.configService.getOrThrow<string>('MAIL_USER'),
+        pass: this.configService.getOrThrow<string>('MAIL_PASSWORD'),
+      },
+    });
+  }
+
+  async sendEmail(dto: MailDto) {
+    const { from, recipients, subject, html } = dto;
+
+    const transport = this.mailTransport();
+
+    const options: MailOptions = {
+      from: from ?? {
+        name: this.configService.getOrThrow<string>('APP_NAME') || 'LogisticApp',
+        address: this.configService.getOrThrow<string>('DEFAULT_EMAIL_FROM') || 'codecrafters@team.com',
+      },
+      to: recipients,
+      subject,
+      html,
+    };
+
+    try {
+      return await transport.sendMail(options);
+    } catch (error) {
+      return InternalServerErrorException;
+    }
+  }
+}
