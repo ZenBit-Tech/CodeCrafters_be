176b610554803bdbc4bf23a5de0e76a01e862e83
Parle648 <sashahoumi@gmail.com>
2024-10-30 15:32:08 +0200

    * Fix: exlude useless controllers from api doc

--- src/common/database/seeds/superadmin-seeding/seeding.controller.ts
+++ src/common/database/seeds/superadmin-seeding/seeding.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Post } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
 
 import { SeedingService } from './seeding.service';
 
+@ApiExcludeController()
 @Controller('seeding')
 export class SeedingController {
   constructor(private readonly seedingService: SeedingService) {}
--- src/common/mailer/mailer.controller.ts
+++ src/common/mailer/mailer.controller.ts
@@ -1,19 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiExcludeController } from '@nestjs/swagger';
 
-import { MailerResponseDto } from './dto/mailer-response.dto';
 import { CreateMailDto } from './dto/send-mail.dto';
 import { MailerService } from './mailer.service';
 
-@ApiBearerAuth()
-@ApiTags('mailer')
+@ApiExcludeController()
 @Controller('mailer')
 export class MailerController {
   constructor(private readonly mailerService: MailerService) {}
 
   @Post('/invitation')
-  @ApiOperation({ summary: 'Sending invitation mail to user' })
-  @ApiResponse({ status: 200, type: MailerResponseDto })
   async sendMail(@Body() mailDto: CreateMailDto) {
     return this.mailerService.sendEmail(mailDto);
   }
