7f3907acff2d6d6480e0f9f6d528208ddf6febd4
Parle648 <sashahoumi@gmail.com>
2024-10-31 13:47:45 +0200

    - Delete: controller for superadmin seed

--- src/common/database/seeds/superadmin-seeding/seeding.module.ts
+++ src/common/database/seeds/superadmin-seeding/seeding.module.ts
@@ -3,12 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'common/database/entities/user.entity';
 import { MailerService } from 'common/mailer/mailer.service';
 
-import { SeedingController } from './seeding.controller';
 import { SeedingService } from './seeding.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [SeedingController],
+  controllers: [],
   providers: [SeedingService, MailerService],
 })
 export class SeedingModule {}
