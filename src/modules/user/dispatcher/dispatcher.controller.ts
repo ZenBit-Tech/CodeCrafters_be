052e924c5c9a6d8b94e9c1c503bd99b6dc27e052
Parle648 <sashahoumi@gmail.com>
2024-11-07 10:20:15 +0200

    * Fix: create UserCompanyGuard

--- src/modules/user/dispatcher/dispatcher.controller.ts
+++ src/modules/user/dispatcher/dispatcher.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Param, Delete, Headers, Patch, UseGuards, SetMetadata } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, UseGuards, SetMetadata } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from 'common/database/entities/user.entity';
 import { Roles } from 'common/enums/enums';
 import { RolesGuard } from 'common/guards/roles.guard';
+import { UserCompanyGuard } from 'common/guards/userCompany.guard';
 import { FailedResponse } from 'common/types/failed-response.dto';
 import { ResponseInterface } from 'common/types/interfaces';
 import { SuccessResponse } from 'common/types/response-success.dto';
@@ -18,46 +19,42 @@ export class DispatcherController {
   constructor(private readonly dispatcherService: DispatcherService) {}
 
   @Post()
-  @UseGuards(RolesGuard)
+  @UseGuards(RolesGuard, UserCompanyGuard)
   @SetMetadata('roles', [Roles.ADMIN])
   @ApiOperation({ summary: 'Create dispatcher' })
   @ApiResponse({ status: 201, type: User })
   @ApiResponse({ status: 400, type: FailedResponse })
-  async create(@Body() createDispatcherDto: CreateDispatcherDto, @Headers() { authorization }: { authorization: string }): Promise<User> {
-    return this.dispatcherService.create(createDispatcherDto, authorization);
+  async create(@Body() createDispatcherDto: CreateDispatcherDto): Promise<User> {
+    return this.dispatcherService.create(createDispatcherDto);
   }
 
   @Get(':id')
-  @UseGuards(RolesGuard)
+  @UseGuards(RolesGuard, UserCompanyGuard)
   @SetMetadata('roles', [Roles.ADMIN])
   @ApiOperation({ summary: 'Get dispatcher' })
   @ApiResponse({ status: 200, type: User })
   @ApiResponse({ status: 400, type: FailedResponse })
-  async findOne(@Param('id') id: string, @Headers() { authorization }: { authorization: string }): Promise<User> {
-    return this.dispatcherService.findOne(+id, authorization);
+  async findOne(@Param('id') id: string): Promise<User> {
+    return this.dispatcherService.findOne(+id);
   }
 
   @Patch(':id')
-  @UseGuards(RolesGuard)
+  @UseGuards(RolesGuard, UserCompanyGuard)
   @SetMetadata('roles', [Roles.ADMIN])
   @ApiOperation({ summary: 'Update dispatcher' })
   @ApiResponse({ status: 200, type: SuccessResponse })
   @ApiResponse({ status: 400, type: FailedResponse })
-  async update(
-    @Param('id') id: string,
-    @Body() updateDispatcherDto: UpdateDispatcherDto,
-    @Headers() { authorization }: { authorization: string },
-  ): Promise<ResponseInterface> {
-    return this.dispatcherService.update(+id, updateDispatcherDto, authorization);
+  async update(@Param('id') id: string, @Body() updateDispatcherDto: UpdateDispatcherDto): Promise<ResponseInterface> {
+    return this.dispatcherService.update(+id, updateDispatcherDto);
   }
 
   @Delete(':id')
-  @UseGuards(RolesGuard)
+  @UseGuards(RolesGuard, UserCompanyGuard)
   @SetMetadata('roles', [Roles.ADMIN])
   @ApiOperation({ summary: 'Delete dispatcher' })
   @ApiResponse({ status: 200, type: SuccessResponse })
   @ApiResponse({ status: 400, type: FailedResponse })
-  async remove(@Param('id') id: string, @Headers() { authorization }: { authorization: string }): Promise<ResponseInterface> {
-    return this.dispatcherService.remove(+id, authorization);
+  async remove(@Param('id') id: string): Promise<ResponseInterface> {
+    return this.dispatcherService.remove(+id);
   }
 }
--- src/modules/user/dispatcher/dispatcher.service.ts
+++ src/modules/user/dispatcher/dispatcher.service.ts
@@ -2,20 +2,11 @@ import { BadRequestException, Injectable, InternalServerErrorException } from '@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Company } from 'common/database/entities/company.entity';
 import { User } from 'common/database/entities/user.entity';
-import { Roles } from 'common/enums/enums';
 import { ResponseInterface } from 'common/types/interfaces';
-import * as jwt from 'jsonwebtoken';
 import { EntityManager, Repository } from 'typeorm';
 
 import { CreateDispatcherDto } from './dto/create-dispatcher.dto';
 import { UpdateDispatcherDto } from './dto/update-dispatcher.dto';
-
-interface AccessTokenInterface extends jwt.Jwt {
-  fullName: string;
-  email: string;
-  role: Roles;
-  company_id: Company;
-}
 
 @Injectable()
 export class DispatcherService {
@@ -27,12 +18,9 @@ export class DispatcherService {
     private readonly entityManager: EntityManager,
   ) {}
 
-  async create(createDispatcherDto: CreateDispatcherDto, authorization: string): Promise<User> {
+  async create(createDispatcherDto: CreateDispatcherDto): Promise<User> {
     try {
       const company = await this.companyRepo.findOneByOrFail({ id: createDispatcherDto.company_id });
-
-      const decodedToken = <AccessTokenInterface>jwt.decode(authorization);
-      if (company.id !== decodedToken.company_id.id) throw new BadRequestException("You doesn't work in this company");
 
       const dispatcher = this.userRepo.create({ ...createDispatcherDto, company_id: company });
       return await this.entityManager.save(dispatcher);
@@ -41,24 +29,17 @@ export class DispatcherService {
     }
   }
 
-  async findOne(id: number, authorization: string): Promise<User> {
+  async findOne(id: number): Promise<User> {
     try {
-      const dispatcher = await this.userRepo.findOneByOrFail({ id });
-
-      const decodedToken = <AccessTokenInterface>jwt.decode(authorization);
-      if (dispatcher.company_id.id !== decodedToken.company_id.id) throw new BadRequestException("You doesn't work in this company");
       return await this.userRepo.findOneByOrFail({ id });
     } catch (error) {
       throw new BadRequestException('There is no such dispatcher');
     }
   }
 
-  async update(id: number, updateDispatcherDto: UpdateDispatcherDto, authorization: string): Promise<ResponseInterface> {
+  async update(id: number, updateDispatcherDto: UpdateDispatcherDto): Promise<ResponseInterface> {
     try {
       const dispatcher = await this.userRepo.findOneByOrFail({ id });
-
-      const decodedToken = <AccessTokenInterface>jwt.decode(authorization);
-      if (dispatcher.company_id.id !== decodedToken.company_id.id) throw new BadRequestException("You doesn't work in this company");
 
       await this.userRepo.update(id, { ...updateDispatcherDto });
       return { status: 200, message: `Dispatcher ${dispatcher.full_name} updated successfully` };
@@ -67,12 +48,9 @@ export class DispatcherService {
     }
   }
 
-  async remove(id: number, authorization: string): Promise<ResponseInterface> {
+  async remove(id: number): Promise<ResponseInterface> {
     try {
       const dispatcher = await this.userRepo.findOneByOrFail({ id });
-
-      const decodedToken = <AccessTokenInterface>jwt.decode(authorization);
-      if (dispatcher.company_id.id !== decodedToken.company_id.id) throw new BadRequestException("You doesn't work in this company");
 
       await this.userRepo.delete(id);
       return { status: 200, message: `Dispatcher ${dispatcher.full_name} account deleted successfully` };
