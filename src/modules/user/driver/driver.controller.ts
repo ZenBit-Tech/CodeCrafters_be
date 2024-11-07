4a84ab78434fe9b0f1ba8f2bbf5c45e8a0a4baf1
Parle648 <sashahoumi@gmail.com>
2024-11-07 10:50:27 +0200

    * Fix: apply user company guard

--- src/modules/user/driver/driver.controller.ts
+++ src/modules/user/driver/driver.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, SetMetadata, Headers } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, SetMetadata } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { User } from 'common/database/entities/user.entity';
 import { Roles } from 'common/enums/enums';
 import { RolesGuard } from 'common/guards/roles.guard';
+import { UserCompanyGuard } from 'common/guards/userCompany.guard';
 import { ResponseInterface } from 'common/types/interfaces';
 
 import { DriverService } from './driver.service';
@@ -14,40 +15,36 @@ export class DriverController {
   constructor(private readonly driverService: DriverService) {}
 
   @Post()
-  @UseGuards(RolesGuard)
+  @UseGuards(RolesGuard, UserCompanyGuard)
   @SetMetadata('roles', [Roles.ADMIN, Roles.DISPATCHER])
   @ApiOperation({ summary: 'Create driver' })
   @ApiResponse({ status: 201, type: User })
-  async create(@Body() createDriverDto: CreateDriverDto, @Headers() { authorization }: { authorization: string }): Promise<User> {
-    return this.driverService.create(createDriverDto, authorization);
+  async create(@Body() createDriverDto: CreateDriverDto): Promise<User> {
+    return this.driverService.create(createDriverDto);
   }
 
   @Get(':id')
-  @UseGuards(RolesGuard)
+  @UseGuards(RolesGuard, UserCompanyGuard)
   @SetMetadata('roles', [Roles.ADMIN, Roles.DISPATCHER])
   @ApiOperation({ summary: 'Get driver' })
   @ApiResponse({ status: 200, type: User })
-  async findOne(@Param('id') id: string, @Headers() { authorization }: { authorization: string }): Promise<User> {
-    return this.driverService.findOne(+id, authorization);
+  async findOne(@Param('id') id: string): Promise<User> {
+    return this.driverService.findOne(+id);
   }
 
   @Patch(':id')
-  @UseGuards(RolesGuard)
+  @UseGuards(RolesGuard, UserCompanyGuard)
   @SetMetadata('roles', [Roles.ADMIN, Roles.DISPATCHER])
   @ApiOperation({ summary: 'Update driver' })
-  async update(
-    @Param('id') id: string,
-    @Body() updateDriverDto: UpdateDriverDto,
-    @Headers() { authorization }: { authorization: string },
-  ): Promise<ResponseInterface> {
-    return this.driverService.update(+id, updateDriverDto, authorization);
+  async update(@Param('id') id: string, @Body() updateDriverDto: UpdateDriverDto): Promise<ResponseInterface> {
+    return this.driverService.update(+id, updateDriverDto);
   }
 
   @Delete(':id')
-  @UseGuards(RolesGuard)
+  @UseGuards(RolesGuard, UserCompanyGuard)
   @SetMetadata('roles', [Roles.ADMIN, Roles.DISPATCHER])
   @ApiOperation({ summary: 'Delete driver' })
-  async remove(@Param('id') id: string, @Headers() { authorization }: { authorization: string }): Promise<ResponseInterface> {
-    return this.driverService.remove(+id, authorization);
+  async remove(@Param('id') id: string): Promise<ResponseInterface> {
+    return this.driverService.remove(+id);
   }
 }
