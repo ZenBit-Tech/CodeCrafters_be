420d28cb3aa143103c5d77c153c01ce5cb53ee23
Parle648 <sashahoumi@gmail.com>
2024-11-11 10:54:22 +0200

    - Delete: invitationToken guard

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,6 @@
-import { BadRequestException, Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { isEmail } from 'class-validator';
-import { InvitationTokenGuard } from 'common/guards/InvitationTokenGuard.guard';
 
 import { AuthService } from './auth.service';
 import { AuthResponseDto } from './dto/auth-response.dto';
@@ -17,13 +16,9 @@ export class AuthController {
   @ApiOperation({ summary: 'Get token by email' })
   @ApiResponse({ status: 200, type: AuthResponseDto })
   @ApiResponse({ status: 400, type: BadRequestResponseDto })
-  @UseGuards(InvitationTokenGuard)
-  async findOne(
-    @Param('email') email: string,
-    @Query() { invitationToken }: { invitationToken: string },
-  ): Promise<{ token: string } | BadRequestException> {
+  async findOne(@Param('email') email: string): Promise<{ token: string } | BadRequestException> {
     if (isEmail(email)) {
-      return this.authService.authByEmail(email, invitationToken);
+      return this.authService.authByEmail(email);
     }
 
     throw new BadRequestException("String isn't email");
