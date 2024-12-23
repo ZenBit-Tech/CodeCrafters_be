bdbda7e0badeca2771117096edd4cee9e343797f
Parle648 <sashahoumi@gmail.com>
2024-12-23 12:14:59 +0200

    * Improove: transformNotifications util

--- src/common/utils/transformNotifications.ts
+++ src/common/utils/transformNotifications.ts
@@ -17,59 +17,40 @@ export interface GetNotificationsResponse {
   thisYear: TransformedNotification[];
 }
 
-export const transformNotifications = (notifications: Notification[]): GetNotificationsResponse => {
-  notifications.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
+const transformNotification = (notification: Notification, diffTime: string) => {
+  return {
+    id: notification.id,
+    type: notification.type,
+    linkText: notification.link_text,
+    linkHref: notification.link_href,
+    message: notification.message,
+    timeDifference: diffTime,
+  };
+};
 
+export const transformNotifications = (notifications: Notification[]): GetNotificationsResponse => {
   const getNotificationsResponse: GetNotificationsResponse = {
     today: [],
     yesterday: [],
     thisMonth: [],
     thisYear: [],
   };
-
   const now = new Date();
 
+  notifications.sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
   notifications.forEach((notification) => {
     const diffTime = Math.floor(Math.abs(now.getTime() - new Date(notification.createdAt).getTime()) / 3600000);
     if (diffTime < 24) {
-      getNotificationsResponse.today.push({
-        id: notification.id,
-        type: notification.type,
-        linkText: notification.link_text,
-        linkHref: notification.link_href,
-        message: notification.message,
-        timeDifference: `${diffTime}h`,
-      });
+      getNotificationsResponse.today.push(transformNotification(notification, `${diffTime}h`));
     }
     if (diffTime >= 24 && diffTime < 48) {
-      getNotificationsResponse.yesterday.push({
-        id: notification.id,
-        type: notification.type,
-        linkText: notification.link_text,
-        linkHref: notification.link_href,
-        message: notification.message,
-        timeDifference: `${diffTime}h`,
-      });
+      getNotificationsResponse.yesterday.push(transformNotification(notification, 'yesterday'));
     }
     if (diffTime >= 48 && diffTime < 720) {
-      getNotificationsResponse.thisMonth.push({
-        id: notification.id,
-        type: notification.type,
-        linkText: notification.link_text,
-        linkHref: notification.link_href,
-        message: notification.message,
-        timeDifference: `${diffTime}h`,
-      });
+      getNotificationsResponse.thisMonth.push(transformNotification(notification, 'this moth'));
     }
     if (diffTime >= 720 && diffTime < 8640) {
-      getNotificationsResponse.thisYear.push({
-        id: notification.id,
-        type: notification.type,
-        linkText: notification.link_text,
-        linkHref: notification.link_href,
-        message: notification.message,
-        timeDifference: `${diffTime}h`,
-      });
+      getNotificationsResponse.thisYear.push(transformNotification(notification, 'this year'));
     }
   });
 
